5f5da4b42e16dde06dae8a3e9b3fa1c2b6c65839
nattalides <nicolas.attalides@gmail.com>
2020-05-24 21:00:01 +0200

    example1

--- Example1/exmple1_script.R
+++ Example1/exmple1_script.R
@@ -1,19 +1,44 @@
+df <- winequality_red
+
+saveRDS(df, "data/data.rds")
+
 library(dplyr)
-
-data(package = "dplyr")
-
-View(starwars)
+library(magrittr)
+library(corrplot)
+library(strignr)
+library(rsample)
 
 # Example 1
 
-# Select columns
-df <- select(starwars, name, height, mass, species)
+# Load and view the data.
+df <- readRDS("data/data.rds")
 
-# Filter rows by height condition
-df <- filter(df, height >= 175)
+View(df)
 
-# Filter rows by species condition
-df <- filter(df, species == "Human")
+# Do some exploratory data analysis (EDA).
+# 1. A summary of the data frame
+df %>% summary
 
-# Arrange rows by descending mass
-df <- arrange(df, desc(mass))
+# 2. A correlation plot of the data
+df %>% cor() %>% corrplot.mixed(upper = "circle",
+                                tl.cex = 1,
+                                tl.pos = 'lt',
+                                number.cex = 0.75)
+
+# Fix column names
+colnames(df) <- df %>% colnames() %>% str_replace_all(pattern = " ",
+                                                      replacement = "_")
+
+# Remove any missing values
+df <- df %>% na.omit()
+
+# Split the data into: a) Train set, b) Test set
+
+set.seed(12345) # Fix randomisation by setting the seed (reproducability)
+# Put 3/4 of the data into the training set 
+data_split <- initial_split(df, prop = 0.8)
+
+# Create data frames for the two sets:
+train_data <- training(data_split)
+
+test_data  <- testing(data_split)
