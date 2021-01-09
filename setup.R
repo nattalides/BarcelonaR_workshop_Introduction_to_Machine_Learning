9665b3433c66e16a6c1a5be6a1ae479c79d63deb
nattalides <nicolas.attalides@gmail.com>
2021-01-09 19:16:22 +0000

    Add installation instructions

---
+++ setup.R
@@ -0,0 +1,15 @@
+install.packages(c("tidyverse", "tidymodels", "rpart", "randomForest", "xgboost"))
+
+library(tidyverse)
+library(tidymodels)
+library(rpart)
+library(randomForest)
+library(xgboost)
+
+#Optional
+install.packages("corrplot", "visNetwork", "visNetwork")
+library(corrplot)
+library(visNetwork)
+library(sparkline)
+
+sessionInfo()
