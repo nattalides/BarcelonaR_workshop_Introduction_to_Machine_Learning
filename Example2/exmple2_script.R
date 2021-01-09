07ccdbc69f35aecfc576ea1c0121d80c8d037242
nattalides <nicolas.attalides@gmail.com>
2021-01-09 19:16:49 +0000

    use . for formula

--- Example2/exmple2_script.R
+++ Example2/exmple2_script.R
@@ -6,7 +6,7 @@
 # a) The target variable (is it numeric or a class?)
 # b) The features of the model
 
-# a) "quality" is the target variable and it is numeric
+# a) "quality" is the target variable and it is numeric (can also be thought of as a class!)
 # b) one or more of the other variables can be model a feature (these are also numeric)
 
 # 2. Design a simple formula to predict the target variable.
@@ -15,6 +15,9 @@
 fmla1 <- formula(quality ~ fixed_acidity + volatile_acidity + citric_acid + 
                    residual_sugar + chlorides + free_sulfur_dioxide + 
                    total_sulfur_dioxide + density + pH + sulphates + alcohol)
+
+# Or the same as above but in shorter format
+fmla1 <- formula(quality ~ .) # The "." says use all available features
 
 # 3. Get creative and engineer some features to design other formulas!
 
